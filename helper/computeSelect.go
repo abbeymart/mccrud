6d30aa7fc9293010430e0567ca5ec9500b9e00a1
Abi Akindele [abbeymart] <2366595+abbeymart@users.noreply.github.com>
2021-01-09 15:41:33 -0500

    refactored select-query methods

--- helper/computeSelect.go
+++ helper/computeSelect.go
@@ -14,21 +14,31 @@ import (
 // ComputeSelectQueryAll compose select SQL script to retrieve all table-records
 // The query may be limit/response may be controlled, by the user, by appending skip and limit options
 func ComputeSelectQueryAll(tableName string, tableFields []string) (string, error) {
-	if tableName == "" || len(tableFields) < 1 {
-		return "", errors.New("table-name and table-fields/columns are required to perform the select operation")
+	if tableName == "" {
+		return "", errors.New("table-name is required to perform the select operation")
 	}
-	selectQuery := fmt.Sprintf("SELECT %v FROM %v", strings.Join(tableFields, ", "), tableName)
+	selectQuery := ""
+	if len(tableFields) > 0 {
+		selectQuery = fmt.Sprintf("SELECT %v FROM %v", strings.Join(tableFields, ", "), tableName)
+	} else {
+		selectQuery = fmt.Sprintf("SELECT * FROM %v", tableName)
+	}
+
 	return selectQuery, nil
 }
 
 // ComputeSelectQueryById compose select SQL script by id(s)
 func ComputeSelectQueryById(tableName string, recordIds []string, tableFields []string) (string, error) {
-	if tableName == "" || len(recordIds) < 1 || len(tableFields) < 1 {
-		return "", errors.New("table-name, record-ids and table-fields/columns are required to perform the select operation")
+	if tableName == "" || len(recordIds) < 1 {
+		return "", errors.New("table-name and record-ids are required to perform the select operation")
 	}
-	// get record(s) based on projected/provided field names ([]string)
-	// select field/column-names
-	selectQuery := fmt.Sprintf("SELECT %v", strings.Join(tableFields, ", "))
+	selectQuery := ""
+	if len(tableFields) > 0 {
+		// get record(s) based on projected/provided field names ([]string)
+		selectQuery = fmt.Sprintf("SELECT %v", strings.Join(tableFields, ", "))
+	} else {
+		selectQuery = fmt.Sprintf("SELECT * FROM %v", tableName)
+	}
 	// from / where condition (where-in-values)
 	selectQuery += fmt.Sprintf(" FROM %v WHERE id IN(%v)", tableName, strings.Join(recordIds, ", "))
 	return selectQuery, nil
@@ -36,14 +46,18 @@ func ComputeSelectQueryById(tableName string, recordIds []string, tableFields []
 
 // ComputeSelectQueryByParam compose SELECT query from the where-parameters
 func ComputeSelectQueryByParam(tableName string, where mctypes.WhereParamType, tableFields []string) (string, error) {
-	if tableName == "" || len(where) < 1 || len(tableFields) < 1 {
-		return "", errors.New("table-name, where-params and table-fields/columns are required to perform the select operation")
+	if tableName == "" || len(where) < 1 {
+		return "", errors.New("table-name and where-params are required to perform the select operation")
 	}
-	// get record(s) based on projected/provided field names ([]string)
-	// select field/column-names
-	selectQuery := fmt.Sprintf("SELECT %v", strings.Join(tableFields, ", "))
-	// from
-	selectQuery += fmt.Sprintf(" FROM %v", tableName)
+	selectQuery := ""
+	if len(tableFields) > 0 {
+		// get record(s) based on projected/provided field names ([]string)
+		selectQuery = fmt.Sprintf("SELECT %v", strings.Join(tableFields, ", "))
+		// from
+		selectQuery += fmt.Sprintf(" FROM %v", tableName)
+	} else {
+		selectQuery = fmt.Sprintf("SELECT * FROM %v", tableName)
+	}
 	// add where-params condition
 	if whereScript, err := ComputeWhereQuery(where, tableFields); err == nil {
 		selectQuery += whereScript
